c881fed12a2a16390e3ed5b41c497d81909fcbcf
student <student@GBTC441J14LX.montgomerycollege.edu>
2017-06-21 11:07:15 -0400

    first commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Greeting {
+        
+	private final long id;
+	private final String content;
+	public Greeting(long id, String content){
+		this.id = id;
+		this.content = content;
+	} 
+	public long getID(){
+		return id;
+	}
+	public String getContent(){
+		return content;
+	}
+	@Override
+	public String toString(){
+		return content;
+	}
+}
---
+++ src/main/java/me/sunil/demo/MyfirstspringprojectApplication.java
@@ -0,0 +1,12 @@
+package me.sunil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringprojectApplication.class, args);
+	}
+}
